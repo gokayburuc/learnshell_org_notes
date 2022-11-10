a79c95a25c1d9d04abf79cdc4636b9a99fb11bdc
Gökay BÜRÜÇ <gokayburuc@yandex.com>
2022-11-10 13:09:59 +0300

    first commit

---
+++ 02_variables/variables.sh
@@ -0,0 +1,10 @@
+
+PRICE_PER_APPLE=5
+
+# use backslash \ to escape special character meaning
+echo 'The price of an Apple today is:\$HK $PRICE_PER_APPLE'
+
+MyFirstLetters='ABCDE'
+greeting='hello world!'
+
+echo 'The first 5 letters in the alphabet are:${MyFirstLetters} and the others}'
